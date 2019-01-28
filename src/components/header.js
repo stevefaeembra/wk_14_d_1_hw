242c5220ce5122462ddd50d50c4df5b2b5a65121
Steven Kay <stevendkay@gmail.com>
2019-01-28 10:56:33 +0000

    added title prop to header

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        I are header, rawr!
+        {this.props.title}
       </div>
     )
   }
--- src/containers/page.js
+++ src/containers/page.js
@@ -14,7 +14,7 @@ class Page extends Component {
   render() {
     return (
       <div className='page'>
-        <Header />
+        <Header title='Upcoming Film Releases for UK' />
         <MovieList />
         <MoreMovies />
       </div>
